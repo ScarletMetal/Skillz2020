02ea22933d955c896d1db8b50656e0e3b78c6044
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-12-19 09:14:45 +0200

    added base bot

---
+++ bot.py
@@ -0,0 +1,34 @@
+"""
+This is an example for a bot.
+"""
+from penguin_game import *
+
+
+def do_turn(game):
+    """
+    Makes the bot run a single turn.
+
+    :param game: the current game state.
+    :type game: Game
+    """
+    # Go over all of my icebergs.
+    for my_iceberg in game.get_my_icebergs():
+        # The amount of penguins in my iceberg.
+        my_penguin_amount = my_iceberg.penguin_amount  # type: int
+
+        # If there are any neutral icebergs.
+        if game.get_neutral_icebergs():
+            # Target a neutral iceberg.
+            destination = game.get_neutral_icebergs()[0]  # type: Iceberg
+        else:
+            # Target an enemy iceberg.
+            destination = game.get_enemy_icebergs()[0]  # type: Iceberg
+
+        # The amount of penguins the target has.
+        destination_penguin_amount = destination.penguin_amount  # type: int
+
+        # If my iceberg has more penguins than the target iceberg.
+        if my_penguin_amount > destination_penguin_amount:
+            # Send penguins to the target.
+            print my_iceberg, "sends", (destination_penguin_amount + 1), "penguins to", destination
+            my_iceberg.send_penguins(destination, destination_penguin_amount + 1)
